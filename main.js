438676beffc165d4a8d9796ad1025e3d7d76b479
Clifford <cliffordatuchukwu92@gmail.com>
2025-06-22 16:52:13 +0200

    Added main.js to render the podcast list, handle the click events

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-// scripts/main.js
-
-import { podcasts, genres } from "./initialData.js";
+import { podcasts, genres, seasons } from "./initialData.js";
 import { renderPodcastList } from "./render.js";
-
+import { openModal } from "./modal.js";
+import { setupModalClose } from "./modal.js";
+/**
+ * Renders the list of podcasts into the container
+ * Adds a click listener to open the modal with the podcast details when the podcast blocked is clicked
+ */
 document.addEventListener("DOMContentLoaded", () => {
   const listContainer = document.getElementById("podcast-list");
   renderPodcastList(podcasts, listContainer, genres);
+
+  listContainer.addEventListener("click", (event) => {
+    const block = event.target.closest(".podcast-block");
+    if (!block) return;
+
+    const podcastId = Number(block.dataset.id);
+    const podcast = podcasts.find((p) => p.id === podcastId);
+    const seasonDetail =
+      seasons.find((s) => s.id === podcastId)?.seasonDetails || [];
+
+    if (podcast) {
+      openModal(podcast, genres, seasonDetail);
+    }
+  });
+  setupModalClose();
 });
